c87fe04c17913abbce06b4972ea58890e2cc1afb
Andy Beck <andy@umich.edu>
2019-10-06 20:31:43 -0400

    Example of a comment

--- basics/helloWorld.rs
+++ basics/helloWorld.rs
@@ -1,3 +1,9 @@
+// This is a comment, and thus is ignored by the compiler
+// These could be used to inform the reader of the code of important details
+// such as the purpose of the code.
+// This program, for example, prints "Hello, world!" to the console.
+// Neat!
+
 fn main() {
     println!("Hello, world!");
 }
